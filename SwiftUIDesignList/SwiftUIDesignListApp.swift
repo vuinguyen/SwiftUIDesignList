bf04179ecbacb3d09cd9ec429d289974a61ea4fa
Vui Nguyen <vui_engineer@yahoo.com>
2023-02-09 18:59:03 -0700

    Initial Commit

---
+++ SwiftUIDesignList/SwiftUIDesignListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDesignListApp.swift
+//  SwiftUIDesignList
+//
+//  Created by Vui Nguyen on 2/9/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDesignListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
