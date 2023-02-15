ad317a07110600b144c951b4fb4a0057167f29e5
Vui Nguyen <vui_engineer@yahoo.com>
2023-02-15 16:29:36 -0700

    default design library added as a local Swift package to the project's group

---
+++ SwiftUIDesignList/LocalPackages/SunfishDesign/README.md
@@ -0,0 +1,3 @@
+# SunfishDesign
+
+This Swift package provides custom design elements to SwiftUI components, such as custom fonts and colors.
---
+++ SwiftUIDesignList/LocalPackages/SunfishDesign/Tests/SunfishDesignTests/SunfishDesignTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SunfishDesign
+
+final class SunfishDesignTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SunfishDesign().text, "Hello, World!")
+    }
+}
