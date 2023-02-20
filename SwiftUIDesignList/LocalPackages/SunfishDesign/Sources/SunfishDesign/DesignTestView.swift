b9af954e3098af98c439dc3244113c1a42bddeb7
Vui Nguyen <vui_engineer@yahoo.com>
2023-02-20 09:17:06 -0700

    Add platforms to Package so newer SwiftUI code can be recognized in test view

---
+++ SwiftUIDesignList/LocalPackages/SunfishDesign/Sources/SunfishDesign/DesignTestView.swift
@@ -0,0 +1,20 @@
+//
+//  DesignTestView.swift
+//  
+//
+//  Created by Vui Nguyen on 2/20/23.
+//
+
+import SwiftUI
+
+struct DesignTestView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct DesignTestView_Previews: PreviewProvider {
+    static var previews: some View {
+        DesignTestView()
+    }
+}
