61e55a97f0a010925b3ad9b138b04d7bc0fee233
Vui Nguyen <vui_engineer@yahoo.com>
2023-02-25 18:23:52 -0700

    apply custom library fonts to main project

--- SwiftUIDesignList/TextDetail.swift
+++ SwiftUIDesignList/TextDetail.swift
@@ -11,16 +11,20 @@ import SunfishDesign
 struct TextDetail: View {
     var body: some View {
         VStack {
-            Text("Apple's brown")
+            Text("Apple's brown / semibold font")
                 .foregroundColor(.brown)
+                .font(.system(size: 25.0, weight: .semibold))
                 .padding([.top], 60)
-            Text("Sunfish's brown")
+            Text("Sunfish's brown / semibold font")
                 .foregroundColor(.sunfishBrown)
+                .font(.sunfish(size: 25.0, weight: .semibold))
             Divider()
-            Text("Apple's yellow")
+            Text("Apple's yellow color / thin font")
                 .foregroundColor(.yellow)
-            Text("Sunfish's yellow")
+                .font(.system(size: 25.0, weight: .thin))
+            Text("Sunfish's yellow color / thin font")
                 .foregroundColor(.sunfishYellow)
+                .font(.sunfish(size: 25.0, weight: .thin))
             Spacer()
         }
 
