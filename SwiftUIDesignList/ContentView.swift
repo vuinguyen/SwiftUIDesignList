e752fce0ad0b525751332dc51d1cce14d8e3c7b3
Vui Nguyen <vui_engineer@yahoo.com>
2023-03-01 17:16:53 -0700

    update main view with updated navigation links, remove commented out code

--- SwiftUIDesignList/ContentView.swift
+++ SwiftUIDesignList/ContentView.swift
@@ -9,16 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-//        VStack {
-//            Image(systemName: "globe")
-//                .imageScale(.large)
-//                .foregroundColor(.accentColor)
-//            Text("Hello, world!")
-//        }
-//        .padding()
         NavigationStack {
             List {
-                NavigationLink("Text", destination: TextDetail())
+                NavigationLink("Text & Foreground Color", destination: TextForegroundColorDetail())
+                NavigationLink("Text & Background Color", destination: TextBackgroundColorDetail())
                 NavigationLink("Shapes", destination: ShapeDetail())
                 NavigationLink("Images", destination: ImageDetail())
             }
