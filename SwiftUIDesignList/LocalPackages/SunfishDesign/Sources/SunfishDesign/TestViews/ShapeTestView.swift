ff76a7ca4a86ca1a352dda5f93cc700a2a9ffb7f
Vui Nguyen <vui_engineer@yahoo.com>
2023-03-03 12:15:53 -0700

    Add test view for Shapes, and refactor other test view for Text

---
+++ SwiftUIDesignList/LocalPackages/SunfishDesign/Sources/SunfishDesign/TestViews/ShapeTestView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Vui Nguyen on 3/3/23.
+//
+
+import SwiftUI
+
+struct ShapeTestView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        ShapeTestView()
+    }
+}
