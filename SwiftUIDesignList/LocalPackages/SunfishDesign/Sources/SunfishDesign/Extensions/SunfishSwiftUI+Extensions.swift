828f72e05884ff7650282487dac5a1fa67135ffb
Vui Nguyen <vui_engineer@yahoo.com>
2023-03-09 11:45:49 -0700

    extend Shape and ShapeStyle to take in custom colors

--- SwiftUIDesignList/LocalPackages/SunfishDesign/Sources/SunfishDesign/Extensions/SunfishSwiftUI+Extensions.swift
+++ SwiftUIDesignList/LocalPackages/SunfishDesign/Sources/SunfishDesign/Extensions/SunfishSwiftUI+Extensions.swift
@@ -92,3 +92,62 @@ extension View {
     }
 }
 
+// MARK: Shape Extension
+
+/// Use this modifier to fill in a Shape with a ShapeStyle that could be a SunfishColor,
+/// and add a stroke border around that Shape, with a ShapeStyle that could be a SunfishColor
+/// Sample Usage:
+/// Circle()
+///     .fill(.sunfishPink, strokeBorder: .sunfishBrown, lineWidth: 10.0)
+///
+/// Reference:
+/// https://www.hackingwithswift.com/quick-start/swiftui/how-to-fill-and-stroke-shapes-at-the-same-time
+extension Shape {
+    public func fill<Fill: ShapeStyle,
+                     Stroke: ShapeStyle>
+    (_ fillStyle: Fill, strokeBorder strokeStyle: Stroke, lineWidth: Double = 1) -> some View {
+        self
+            .stroke(strokeStyle, lineWidth: lineWidth)
+            .background(self.fill(fillStyle))
+    }
+}
+
+// MARK: ShapeStyle Extension
+
+/// Extend ShapeStyle so that View Modifiers that take a ShapeStyle,
+/// can take a ShapeStyle that is a custom SunfishColor
+///
+/// Sample Usage with the background View Modifier:
+/// Text("Hello, World!")
+///    .background(.sunfishBlue)
+///
+/// Sample Usage with the foregroundColor View Modifier:
+/// Text("Hello, World!")
+///    .foregroundColor(.sunfishPink)
+///
+/// Sample Usage with the fill View Modifier, to fill in a Shape,
+/// with a ShapeStyle that is a SunfishColor:
+/// Rectangle()
+///     .fill(.sunfishBlue)
+///
+/// Sample Usage with the stroke View Modifier, to add a stroke border around a Shape,
+/// with a ShapeStyle that is a SunfishColor:
+/// Ellipse()
+///     .stroke(.sunfishBlue, lineWidth: 5.0)
+extension ShapeStyle where Self == Color {
+    public static var sunfishBlue: Color {
+        Color(SunfishColor.sunfishBlue)
+    }
+    public static var sunfishBrown: Color {
+        Color(SunfishColor.sunfishBrown)
+    }
+    public static var sunfishPink: Color {
+        Color(SunfishColor.sunfishPink)
+    }
+    public static var sunfishTeal: Color {
+        Color(SunfishColor.sunfishTeal)
+    }
+    public static var sunfishYellow: Color {
+        Color(SunfishColor.sunfishYellow)
+    }
+}
