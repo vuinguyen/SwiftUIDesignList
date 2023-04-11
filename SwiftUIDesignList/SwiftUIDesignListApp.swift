0c2bc893874f0ed93fa0f8c1025d9111462a805c
Vui Nguyen <vui_engineer@yahoo.com>
2023-04-11 14:13:46 -0600

    move Views into folder

--- SwiftUIDesignList/SwiftUIDesignListApp.swift
+++ SwiftUIDesignList/SwiftUIDesignListApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SwiftUIDesignListApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainListView()
         }
     }
 }
--- SwiftUIDesignList/Views/MainListView.swift
+++ SwiftUIDesignList/Views/MainListView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MainListView: View {
     var body: some View {
         NavigationStack {
             List {
@@ -22,6 +22,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        MainListView()
     }
 }
