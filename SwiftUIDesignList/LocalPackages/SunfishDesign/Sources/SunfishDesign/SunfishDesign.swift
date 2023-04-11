92af631ffa1dea8c8aed1e30398f9a27e5d9e937
Vui Nguyen <vui_engineer@yahoo.com>
2023-04-11 14:19:26 -0600

    remove unused file & test

--- SwiftUIDesignList/LocalPackages/SunfishDesign/Sources/SunfishDesign/SunfishDesign.swift
+++
@@ -1,6 +0,0 @@
-public struct SunfishDesign {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
--- SwiftUIDesignList/LocalPackages/SunfishDesign/Tests/SunfishDesignTests/SunfishDesignTests.swift
+++ SwiftUIDesignList/LocalPackages/SunfishDesign/Tests/SunfishDesignTests/SunfishDesignTests.swift
@@ -3,13 +3,6 @@ import SwiftUI
 @testable import SunfishDesign
 
 final class SunfishDesignTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(SunfishDesign().text, "Hello, World!")
-    }
-
     // test that all colors from the Assets file can be found and
     // create non-nil Colors
     func testSunfishColorsFromBundle() {
